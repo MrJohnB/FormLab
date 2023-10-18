024b3b96118ee5c3e3eab85c061034c19ea1f547
MrJohnB <jmbianchi@gmail.com>
2023-10-18 12:14:16 -0600

    Added a new database migration to rename the tables from: FormMetadata & FieldMetadata to: FormDefinition & FieldDefinition.

---
+++ src/LiteBulb.FormLab.Infrastructure.Migrations/Migrations/20231018181053_RenameMetadataToDefinition.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LiteBulb.FormLab.Infrastructure.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameMetadataToDefinition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FieldMetadata");
+
+            migrationBuilder.DropTable(
+                name: "FormMetadata");
+
+            migrationBuilder.CreateTable(
+                name: "FormDefinition",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Created = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false),
+                    LastModified = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormDefinition", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "FieldDefinition",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Type = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    FormDefinitionId = table.Column<int>(type: "int", nullable: true),
+                    Created = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false),
+                    LastModified = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FieldDefinition", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FieldDefinition_FormDefinition_FormDefinitionId",
+                        column: x => x.FormDefinitionId,
+                        principalTable: "FormDefinition",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FieldDefinition_FormDefinitionId",
+                table: "FieldDefinition",
+                column: "FormDefinitionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FieldDefinition");
+
+            migrationBuilder.DropTable(
+                name: "FormDefinition");
+
+            migrationBuilder.CreateTable(
+                name: "FormMetadata",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Created = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false),
+                    LastModified = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormMetadata", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "FieldMetadata",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Created = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false),
+                    Description = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    FormMetadataId = table.Column<int>(type: "int", nullable: true),
+                    LastModified = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Type = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FieldMetadata", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FieldMetadata_FormMetadata_FormMetadataId",
+                        column: x => x.FormMetadataId,
+                        principalTable: "FormMetadata",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FieldMetadata_FormMetadataId",
+                table: "FieldMetadata",
+                column: "FormMetadataId");
+        }
+    }
+}
