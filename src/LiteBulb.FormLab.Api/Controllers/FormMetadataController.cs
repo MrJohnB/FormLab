2b9bb750ceee96248ac5dd5deb123abeee2a5a1f
MrJohnB <jmbianchi@gmail.com>
2023-10-18 13:39:45 -0600

    Added an API project with empty controllers (which is the REST API web service for the CRUD database operations).

---
+++ src/LiteBulb.FormLab.Api/Controllers/FormMetadataController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LiteBulb.FormLab.Api.Controllers;
+public class FormMetadataController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
